69742ccec4fce3420df43ecff033188824b9a94c
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-18 22:04:24 -0200

    first

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotationDemoApp {
+public class AnnotationDemoApp { 
 
 	public static void main(String[] args) {
 		// read spring config file 
