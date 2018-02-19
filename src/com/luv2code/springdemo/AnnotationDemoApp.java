ea778e02ffd49555bf776c184b12c6639bf0f5de
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-19 01:04:10 -0200

    first

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -1,7 +1,7 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+ 
 public class AnnotationDemoApp { 
 
 	public static void main(String[] args) {
