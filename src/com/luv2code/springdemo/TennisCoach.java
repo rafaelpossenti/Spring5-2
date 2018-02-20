22e802c33c2c4c9bc0bbeb640c816dd8c38fd235
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-20 01:14:48 -0200

    Qualifier annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,17 +1,40 @@
 package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 	
+	public TennisCoach() {
+		System.out.println("inside default constructor");
+	}
+	
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("inside set Injection");
+		this.fortuneService = fortuneService;
+	}
+	
+	@Autowired
+	public void doAnything(FortuneService fortuneService) {
+		System.out.println("inside the method doAnything");
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	/*
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		 this.fortuneService = fortuneService;
 		
 	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
