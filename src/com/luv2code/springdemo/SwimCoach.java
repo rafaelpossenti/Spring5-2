061de1dbacf91dcd3094b78e83d300e40f4e0f4c
Rafael Possenti <rafael_possenti@hotmail.com>
2018-02-23 00:02:25 -0200

    Inject value

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,17 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService; 
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+	
 	
 	public SwimCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
@@ -17,5 +26,16 @@ public class SwimCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
+	
 
 }
